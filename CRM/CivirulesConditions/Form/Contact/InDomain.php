32aa3b3219da4c85924f57ac7398465cdaf64efc
Matthew Wire <mjw@mjwconsult.co.uk>
2022-08-18 13:52:32 +0100

    Fix #182 Domain conditional selection shows a maximum of 25 domains

--- CRM/CivirulesConditions/Form/Contact/InDomain.php
+++ CRM/CivirulesConditions/Form/Contact/InDomain.php
@@ -12,7 +12,6 @@ class CRM_CivirulesConditions_Form_Contact_InDomain extends CRM_CivirulesConditi
    * Method to get groups
    *
    * @return array
-   * @access protected
    */
   protected function getDomains() {
     return CRM_CivirulesConditions_Contact_InDomain::domains();
@@ -22,7 +21,6 @@ class CRM_CivirulesConditions_Form_Contact_InDomain extends CRM_CivirulesConditi
    * Method to get operators
    *
    * @return array
-   * @access protected
    */
   protected function getOperators() {
     return CRM_CivirulesConditions_Contact_InDomain::getOperatorOptions();
@@ -31,24 +29,23 @@ class CRM_CivirulesConditions_Form_Contact_InDomain extends CRM_CivirulesConditi
   /**
    * Overridden parent method to build form
    *
-   * @access public
    */
   public function buildQuickForm() {
     $this->add('hidden', 'rule_condition_id');
 
-    $group = $this->add('select', 'domain_id', ts('Domain'), $this->getDomains(), true);
+    $this->add('select', 'domain_id', ts('Domain'), $this->getDomains(), true);
     $this->add('select', 'operator', ts('Operator'), $this->getOperators(), true);
 
-    $this->addButtons(array(
-      array('type' => 'next', 'name' => ts('Save'), 'isDefault' => TRUE,),
-      array('type' => 'cancel', 'name' => ts('Cancel'))));
+    $this->addButtons([
+      ['type' => 'next', 'name' => ts('Save'), 'isDefault' => TRUE,],
+      ['type' => 'cancel', 'name' => ts('Cancel')]
+    ]);
   }
 
   /**
    * Overridden parent method to set default values
    *
    * @return array $defaultValues
-   * @access public
    */
   public function setDefaultValues() {
     $defaultValues = parent::setDefaultValues();
@@ -66,7 +63,6 @@ class CRM_CivirulesConditions_Form_Contact_InDomain extends CRM_CivirulesConditi
    * Overridden parent method to process form data after submission
    *
    * @throws Exception when rule condition not found
-   * @access public
    */
   public function postProcess() {
     $data['domain_id'] = $this->_submitValues['domain_id'];
