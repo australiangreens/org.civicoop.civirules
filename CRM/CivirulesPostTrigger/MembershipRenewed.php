621bf55030a51176a78ad903e29554eb35428e85
Matthew Wire <mjw@mjwconsult.co.uk>
2023-09-26 17:15:08 +0100

    Add additional entity data (contribution) to membership trigger if available

--- CRM/CivirulesPostTrigger/MembershipRenewed.php
+++ CRM/CivirulesPostTrigger/MembershipRenewed.php
@@ -5,25 +5,7 @@
  * @license AGPL-3.0
  */
 
-class CRM_CivirulesPostTrigger_MembershipRenewed extends CRM_Civirules_Trigger_Post {
-
-  /**
-   * Returns an array of entities on which the trigger reacts
-   *
-   * @return CRM_Civirules_TriggerData_EntityDefinition
-   */
-  protected function reactOnEntity() {
-    return new CRM_Civirules_TriggerData_EntityDefinition($this->objectName, $this->objectName, $this->getDaoClassName(), 'Membership');
-  }
-
-  /**
-   * Return the name of the DAO Class. If a dao class does not exist return an empty value
-   *
-   * @return string
-   */
-  protected function getDaoClassName() {
-    return 'CRM_Member_DAO_Membership';
-  }
+class CRM_CivirulesPostTrigger_MembershipRenewed extends CRM_CivirulesPostTrigger_Membership {
 
   /**
    * Trigger a rule for this trigger
