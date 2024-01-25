3bff134899c8309479234e0e8993fdef4b0035af
Matthew Wire <mjw@mjwconsult.co.uk>
2024-01-25 11:24:44 +0000

    Convert to API4 and add description for Days since Last Case Activity condition

--- CRM/CivirulesConditions/Form/Case/CaseActivity.php
+++ CRM/CivirulesConditions/Form/Case/CaseActivity.php
@@ -1,5 +1,10 @@
 <?php
 
+use CRM_Civirules_ExtensionUtil as E;
+
+/**
+ * Form Class for "Days since Last Case Activity" condition
+ */
 class CRM_CivirulesConditions_Form_Case_CaseActivity extends CRM_CivirulesConditions_Form_Form {
 
   protected function getCaseStatus() {
@@ -9,23 +14,20 @@ class CRM_CivirulesConditions_Form_Case_CaseActivity extends CRM_CivirulesCondit
   /**
    * Overridden parent method to build form
    *
-   * @access public
    */
   public function buildQuickForm() {
     $this->add('hidden', 'rule_condition_id');
-
-    $this->add('text', 'days_inactive', ts('Number of days'), array(), true);
-
-    $this->addButtons(array(
-      array('type' => 'next', 'name' => ts('Save'), 'isDefault' => TRUE,),
-      array('type' => 'cancel', 'name' => ts('Cancel'))));
+    $this->add('text', 'days_inactive', E::ts('Number of days'), [], TRUE);
+    $this->addButtons([
+      ['type' => 'next', 'name' => ts('Save'), 'isDefault' => TRUE,],
+      ['type' => 'cancel', 'name' => ts('Cancel')]
+    ]);
   }
 
   /**
    * Overridden parent method to set default values
    *
    * @return array $defaultValues
-   * @access public
    */
   public function setDefaultValues() {
     $defaultValues = parent::setDefaultValues();
@@ -40,7 +42,6 @@ class CRM_CivirulesConditions_Form_Case_CaseActivity extends CRM_CivirulesCondit
    * Overridden parent method to process form data after submission
    *
    * @throws Exception when rule condition not found
-   * @access public
    */
   public function postProcess() {
     $data['days_inactive'] = $this->_submitValues['days_inactive'];
@@ -48,4 +49,5 @@ class CRM_CivirulesConditions_Form_Case_CaseActivity extends CRM_CivirulesCondit
     $this->ruleCondition->save();
     parent::postProcess();
   }
+
 }
