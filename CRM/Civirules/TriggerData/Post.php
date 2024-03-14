c9ea5f85b7744bb717e1af03367a39c039711e76
Matthew Wire <mjw@mjwconsult.co.uk>
2024-03-14 13:08:26 +0000

    Add 'Update Recurring Contribution Status' action and make it work with Membership trigger

--- CRM/Civirules/TriggerData/Post.php
+++ CRM/Civirules/TriggerData/Post.php
@@ -1,5 +1,7 @@
 <?php
 
+use Civi\Api4\Membership;
+
 class CRM_Civirules_TriggerData_Post extends CRM_Civirules_TriggerData_TriggerData {
 
   /**
@@ -14,26 +16,32 @@ class CRM_Civirules_TriggerData_Post extends CRM_Civirules_TriggerData_TriggerDa
     parent::__construct();
     $this->setEntity($entity);
     $this->setEntityId($objectId);
-    if ($entity === 'Contact') {
-      $this->setContactId($objectId);
+
+    // When we are triggered via a Post hook we are not guaranteed to have all values
+    // for the entity. Maybe we should load them all here?
+    switch ($entity) {
+      case 'Contact':
+        $this->setContactId($objectId);
+        $this->setEntityData($entity, $data);
+        break;
+
+      case 'Membership':
+        // Load the membership entity (this makes sure we have all fields such as contribution_recur_id if set).
+        $membership = Membership::get(FALSE)
+          ->addWhere('id', '=', $objectId)
+          ->execute()
+          ->first();
+        $this->setContactId($membership['contact_id']);
+        $this->setEntityData('Membership', $membership);
+        break;
+
+      default:
+        // Generic handler: Just make sure contactID is set.
+        if (isset($data['contact_id'])) {
+          $this->setContactId($data['contact_id']);
+        }
+        $this->setEntityData($entity, $data);
     }
-    elseif (isset($data['contact_id'])) {
-      $this->setContactId($data['contact_id']);
-    }
-    else {
-      switch ($entity) {
-        case 'Membership':
-          $this->setContactId(
-            civicrm_api3('Membership', 'getvalue', [
-            'return' => 'contact_id',
-            'id' => $objectId,
-            ])
-          );
-          $data['contact_id'] = $this->getContactId();
-          break;
-      }
-    }
-    $this->setEntityData($entity, $data);
   }
 
 }
