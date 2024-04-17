0d6360b826160eb70fcfd6188b42314258d929bb
Matthew Wire <mjw@mjwconsult.co.uk>
2024-04-17 10:33:53 +0100

    Cleanup API3 Civirules.cron

--- api/v3/Civirules/Cron.php
+++ api/v3/Civirules/Cron.php
@@ -5,8 +5,8 @@
  * This is used for documentation and validation.
  *
  * @param array $spec description of fields supported by this API call
+ *
  * @return void
- * @see http://wiki.civicrm.org/confluence/display/CRM/API+Architecture+Standards
  */
 function _civicrm_api3_civirules_cron_spec(&$spec) {
   //there are no parameters for the civirules cron
@@ -16,27 +16,28 @@ function _civicrm_api3_civirules_cron_spec(&$spec) {
  * Civirules.Cron API
  *
  * @param array $params
+ *
  * @return array API result descriptor
  * @see civicrm_api3_create_success
  * @see civicrm_api3_create_error
- * @throws API_Exception
+ * @throws \CRM_Core_Exception
  */
 function civicrm_api3_civirules_cron($params) {
-  $returnValues = array();
+  $returnValues = [];
 
   //prevent from crashing with a max execution time error
   set_time_limit(0);
 
-  $rules = CRM_Civirules_BAO_Rule::findRulesForCron();
+  $rules = CRM_Civirules_BAO_CiviRulesRule::findRulesForCron();
   foreach($rules as $rule) {
     $return = $rule->process();
     $triggeredEntities = $return['count'];
     $triggeredActions = $return['is_valid_count'];
-    $returnValues[$rule->getRuleId()] = array(
-      'rule' => CRM_Civirules_BAO_Rule::getRuleLabelWithId($rule->getRuleId()),
+    $returnValues[$rule->getRuleId()] = [
+      'rule' => CRM_Civirules_BAO_CiviRulesRule::getRuleLabelWithId($rule->getRuleId()),
       'triggered_entities' => $triggeredEntities,
       'triggered_actions' => $triggeredActions,
-    );
+    ];
   }
 
   return civicrm_api3_create_success($returnValues, $params, 'Civirules', 'cron');
