777f5b65877e02f4b6ad4c0892e7dc932f9f1c2d
Matthew Wire (MJW) <mjw@mjwconsult.co.uk>
2025-05-14 11:54:28 +0100

    Following https://lab.civicrm.org/extensions/civirules/-/commit/d25d13353313e109e5989c3738dccc9b06fd3037 execute conditions by weight

--- CRM/Civirules/Trigger.php
+++ CRM/Civirules/Trigger.php
@@ -177,7 +177,7 @@ abstract class CRM_Civirules_Trigger {
     if (!isset($this->ruleConditions) && !empty($this->ruleId)) {
       $this->ruleConditions = \Civi\Api4\CiviRulesRuleCondition::get(FALSE)
         ->addWhere('rule_id', '=', $this->ruleId)
-        // ->addOrderBy('weight', 'DESC') - see !https://lab.civicrm.org/extensions/civirules/-/issues/258
+        ->addOrderBy('weight', 'ASC')
         ->addOrderBy('id', 'ASC')
         ->execute()
         ->getArrayCopy();
